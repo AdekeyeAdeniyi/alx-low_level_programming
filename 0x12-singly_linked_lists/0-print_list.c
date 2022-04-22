8c29ae2800e73ecbb18c9b12bedc77c691c8b172
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-04-21 17:57:44 -0700

    update 0-print_list code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,17 @@
   */
 size_t print_list(const list_t *h)
 {
-	int count;
+	size_t count;
 
 	count = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			h = h->next;
-			count++;
-		}
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		count++;
+		if(h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
 
 	return (count);
