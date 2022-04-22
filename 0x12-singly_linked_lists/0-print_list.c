b0d4537b914d4670fb2b76812e82fa9fe5128669
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-04-21 18:00:07 -0700

    singly list data structure code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
-  *print_list - prints all the elements of a list_t.
-  *@h: pointer to list.
-  *
-  *Return: the number of nodes.
-  */
+ * print_list - Prints all elements of a list
+ * @h: A linked list
+ *
+ * Return: The number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	count = 0;
 	while (h != NULL)
 	{
-		count++;
-		if(h->str == NULL)
-			printf("[%d] (nil)\n", 0);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
+		count++;
 	}
 
 	return (count);
