d6c7178e24464a0ffc56fa32123faf4f0cbba4be
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-06-16 23:16:52 +0100

    create a shared library for math operations

--- 0x18-dynamic_libraries/test/div.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int div(int num1, int num2){
-	int sum = num1 / num2;
-	return sum;
-}
--- 0x18-dynamic_libraries/test/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -fPIC -shared -o 100-operations.so *.c
-gcc -fPIC -shared -o 100-operations.dll *.c
