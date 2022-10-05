acb58b9e91ed61493eb3eb241113d6540b5ad671
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-05 14:42:06 +0100

    linear algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,39 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "search_algos.h"
-
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
-}
 /**
 * Function: linear_search
 * ----------------------------
-*   Return the first index where value is located
+* Return the first index where value is located
 *
-*   array(int): sets of integer
-*   size(int): size of array
-*   value(int): value to search for in array
+* @array(int): sets of integer
+* @size(int): size of array
+* @value(int): value to search for in array
 *
-*   returns: Index of value in array
-**/
+* Returns: Index of value in array
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
+	if (array == NULL)
+		return (-1);
+	
 	while (i < size)
 	{
 		printf("Value checked array[%i] = [%i] \n", i, array[i]);
